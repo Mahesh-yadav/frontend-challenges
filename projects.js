a324946aa869b3cce8fdfc343c356eae9feab104
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2020-07-06 17:01:35 +0530

    updates project list page

---
+++ projects.js
@@ -0,0 +1,132 @@
+const projects = [
+    {
+        id: 1,
+        title: 'clipboard landing page',
+        technologies: ['HTML', 'CSS'],
+        designUrl: './designs/clipboard-landing-page.jpg',
+        url: './clipboard-landing-page/index.html'
+        
+    },
+    {
+        id: 2,
+        title: 'fylo two column layout',
+        technologies: ['HTML', 'CSS'],
+        designUrl: './designs/fylo-two-column-layout.jpg',
+        url: './fylo-two-column-layout/index.html'
+    },
+    {
+        id: 3,
+        title: 'huddle page curved sections',
+        technologies: ['HTML', 'CSS'],
+        designUrl: './designs/huddle-landing-page-curved.jpg',
+        url: './huddle-landing-page-with-curved-sections/index.html'
+    },
+    {
+        id: 4,
+        title: 'insure landing page',
+        technologies: ['HTML', 'CSS'],
+        designUrl: './designs/insure-landing-page.jpg',
+        url: './insure-landing-page/index.html'
+    },
+    {
+        id: 5,
+        title: 'social media dashboard',
+        technologies: ['HTML', 'CSS'],
+        designUrl: './designs/social-media-theme-switcher.jpg',
+        url: './social-media-dashboard-with-theme-switcher/index.html'
+    },
+    {
+        id: 16,
+        title: 'Fylo dark theme page',
+        technologies: ['HTML', 'CSS'],
+        designUrl: './designs/fylo-dark-theme.jpg',
+        url: './fylo-dark-theme-landing-page/index.html'
+    },
+    {
+        id: 10,
+        title: 'huddle alternating blocks',
+        technologies: ['HTML', 'CSS'],
+        designUrl: './designs/huddle-alternating-feature-blocks.jpg',
+        url: './huddle-landing-page-with-alternating-fearture-blocks/index.html'
+    },
+    {
+        id: 6,
+        title: 'basel apparel coming soon',
+        technologies: ['HTML', 'CSS', 'JS'],
+        designUrl: './designs/basel-apparel-coming-soon.jpg',
+        url: './base-apparel-coming-soon/index.html'
+    },
+    {
+        id: 12,
+        title: 'four card feature section',
+        technologies: ['HTML', 'CSS'],
+        designUrl: './designs/four-card-section.jpg',
+        url: './four-card-feature-section/index.html'
+    },
+    {
+        id: 7,
+        title: 'intro signup form',
+        technologies: ['HTML', 'CSS', 'JS'],
+        designUrl: './designs/intro-component-signup.jpg',
+        url: './intro-component-with-signup-form/index.html'
+    },
+    {
+        id: 8,
+        title: 'ping coming soon page',
+        technologies: ['HTML', 'CSS', 'JS'],
+        designUrl: './designs/ping-coming-soon-page.jpg',
+        url: './ping-coming-soon-page/index.html'
+    },
+    {
+        id: 9,
+        title: 'Testimonials slider',
+        technologies: ['HTML', 'CSS', 'JS'],
+        designUrl: './designs/testimonials-slider.jpg',
+        url: './coding-bootcamp-testimonials-slider/index.html'
+    },
+    {
+        id: 11,
+        title: 'Huddle Intro Page',
+        technologies: ['HTML', 'CSS'],
+        designUrl: './designs/huddle-intro-page.jpg',
+        url: './huddle-landing-page-with-single-intro-section/index.html'
+    },
+    {
+        id: 13,
+        title: 'Article preview component',
+        technologies: ['HTML', 'CSS', 'JS'],
+        designUrl: './designs/article-preview.jpg',
+        url: './article-preview-component/index.html'
+    },
+    {
+        id: 14,
+        title: 'pricing component with toggle',
+        technologies: ['HTML', 'CSS', 'JS'],
+        designUrl: './designs/pricing-component-toggle.jpg',
+        url: './pricing-component-with-toggle/index.html'
+    },
+    {
+        id: 15,
+        title: 'project tracking component',
+        technologies: ['HTML', 'CSS', 'JS'],
+        designUrl: './designs/project-tracking-component.jpg',
+        url: './project-tracking-intro-component/index.html'
+    },
+    {
+        id: 17,
+        title: 'grand hotel',
+        technologies: ['HTML', 'CSS'],
+        designUrl: './designs/grand-hotel.png',
+        url: './grand-hotel/index.html'
+    },
+    {
+        id: 18,
+        title: 'furniture store',
+        technologies: ['HTML', 'CSS'],
+        designUrl: './designs/furniture-store.png',
+        url: './furniture-store/index.html'
+    }
+
+];
+
+export default projects;
